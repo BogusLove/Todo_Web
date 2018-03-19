a5d89ce2fc564ccc7960c87381eaa6d3eb92c37d
BogusLove <boguscompany@ukr.net>
2018-03-20 00:04:36 +0200

    create request

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import request from 'superagent';
 
 class App extends Component {
-  render() {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      tasks: null,
+      err: null
+    };
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    return true;
+  }
+  componentDidMount(){
+    request
+    .get('http://localhost:8080/tasks')
+    .then(response => {          
+      this.setState({tasks: JSON.stringify(response.body)}
+    )})
+    .catch(err => {
+      this.setState({err: JSON.stringify(err)});
+    });
+  }
+  render() {    
+    console.log(this.state.tasks);
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App">  
+        {this.state.err ? this.state.err : this.state.tasks ? this.state.tasks.map(item => {return item.task}) : 'Nothing'}
       </div>
     );
   }
